2e59b5dd04db9655d15ec1507c46274af460ec6d
Mohan Kumar N <125173156+MohanReddy05@users.noreply.github.com>
2025-11-09 16:35:40 +0530

    Add truncation and data loading
    Added truncation and loading of additional tables from CSV files.

--- scrips/bronze/load_bronze.sql
+++ scrips/bronze/load_bronze.sql
@@ -8,9 +8,12 @@ SHOW GLOBAL VARIABLES LIKE 'local_infile';
 
 
 /*
+ Truncate tables if data exists
  Load data from csv files onto tables of bronze database
  Show all the items in the tables
 */
+TRUNCATE TABLE bronze.crm_customer_info;
+
 LOAD DATA LOCAL INFILE 'datasets/crm/cutomer_info.csv'
 INTO TABLE bronze.crm_customer_info
 FIELDS TERMINATED BY ',' 
@@ -20,6 +23,8 @@ IGNORE 1 ROWS;
 
 SELECT * FROM bronze.crm_customer_info;
 
+
+TRUNCATE TABLE bronze.crm_product_info;
 
 LOAD DATA LOCAL INFILE 'datasets/crm/product_info.csv'
 INTO TABLE bronze.crm_product_info
@@ -31,6 +36,8 @@ IGNORE 1 ROWS;
 SELECT * FROM bronze.crm_product_info;
 
 
+TRUNCATE TABLE bronze.crm_sales_details;
+
 LOAD DATA LOCAL INFILE 'datasets/crm/sales_details.csv'
 INTO TABLE bronze.crm_sales_details
 FIELDS TERMINATED BY ',' 
@@ -40,6 +47,7 @@ IGNORE 1 ROWS;
 
 SELECT * FROM bronze.crm_sales_details;
 
+TRUNCATE TABLE bronze.erp_cust_az12;
 
 LOAD DATA LOCAL INFILE 'datasets/erp/CUST_AZ12.csv'
 INTO TABLE bronze.erp_cust_az12
@@ -51,6 +59,8 @@ IGNORE 1 ROWS;
 SELECT * FROM bronze.erp_cust_az12;
 
 
+TRUNCATE TABLE bronze.erp_loc_a101;
+
 LOAD DATA LOCAL INFILE 'datasets/erp/LOC_A101.csv'
 INTO TABLE bronze.erp_loc_a101
 FIELDS TERMINATED BY ',' 
@@ -61,6 +71,8 @@ IGNORE 1 ROWS;
 SELECT * FROM bronze.erp_loc_a101;
 
 
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
 LOAD DATA LOCAL INFILE 'datasets/erp/PX_CAT_G1V2.csv'
 INTO TABLE bronze.erp_px_cat_g1v2
 FIELDS TERMINATED BY ',' 
@@ -69,5 +81,6 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 SELECT * FROM bronze.erp_px_cat_g1v2;
+
 
 
